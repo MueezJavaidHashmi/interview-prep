ed45b21b1c43e2183d4641e00626939da603df46
MueezJavaidHashmi <mjh1230689@gmail.com>
2024-12-30 22:07:11 +0500

    feat(remove-element): make a remove element function using two pointer approach with time complexity O(N) and space complexity O(1)

---
+++ problem-solving/remove-element.ts
@@ -0,0 +1,16 @@
+// Using Two Pointer Approach
+const removeElement = (nums: number[], val: number): number => {
+  let rightIndex = nums.length - 1;
+  let leftIndex = 0;
+  while (leftIndex <= rightIndex) {
+    if (nums[leftIndex] === val) {
+      swap(nums, leftIndex, rightIndex);
+      rightIndex--;
+    } else leftIndex++;
+  }
+  return rightIndex + 1;
+};
+
+const swap = (nums: number[], index1: number, index2: number): void => {
+  [nums[index1], nums[index2]] = [nums[index2], nums[index1]];
+}
