428e1cfc2da6d2883950cc6d142464cafe92a186
MueezJavaidHashmi <mjh1230689@gmail.com>
2024-12-31 01:03:37 +0500

    feat(best-time-to-buy-and-sell-stocks-ii): make a max profit function using greedy algorithm with time complexity O(N) and space complexity O(1)

---
+++ problem-solving/best-time-to-buy-and-sell-stocks-ii.ts
@@ -0,0 +1,9 @@
+const maxProfit = (prices: number[]): number => {
+  let maximumProfit = 0;
+
+  for (let i = 1; i < prices.length; i++) {
+    if (prices[i] > prices[i - 1]) maximumProfit += prices[i] - prices[i - 1];
+  }
+
+  return maximumProfit;
+}
