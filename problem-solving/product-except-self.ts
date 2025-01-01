016b50c859cb2ab06fa5924562a5b0193d93fd9c
MueezJavaidHashmi <mjh1230689@gmail.com>
2025-01-01 14:09:44 +0500

    refactor(product-except-self): make solution cleaner and more efficient

--- problem-solving/product-except-self.ts
+++ problem-solving/product-except-self.ts
@@ -2,19 +2,15 @@
 // Time Complexity --> O(N)
 // Space Complexity -> O(1) [answer array does not count]
 const productExceptSelf = (nums: number[]): number[] => {
-  const n = nums.length;
-  const answer: number[] = new Array(n).fill(1);
+  const answer: number[] = new Array(nums.length).fill(1);
 
   let cumulativeLeftProduct = 1;
-  for (let i = 1; i < n; i++) {
-    cumulativeLeftProduct *= nums[i - 1];
-    answer[i] *= cumulativeLeftProduct;
-  }
-
   let cumulativeRightProduct = 1;
-  for (let i = n - 2; i >= 0; i--) {
-    cumulativeRightProduct *= nums[i + 1];
-    answer[i] *= cumulativeRightProduct;
+  for (let i = 0; i < nums.length; i++) {
+    answer[i] *= cumulativeLeftProduct;
+    answer[nums.length - 1 - i] *= cumulativeRightProduct;
+    cumulativeLeftProduct *= nums[i];
+    cumulativeRightProduct *= nums[nums.length - 1 - i];
   }
 
   return answer;
