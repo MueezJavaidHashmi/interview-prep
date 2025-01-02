ccce36bb4d53494bb72731d363661d96a8e1e4ad
MueezJavaidHashmi <mjh1230689@gmail.com>
2025-01-02 19:49:24 +0500

    refactor(merge-sorted-array): refactor merge sorted array function using for loop instead of while loop

--- problem-solving/merge-sorted-array.ts
+++ problem-solving/merge-sorted-array.ts
@@ -2,11 +2,10 @@
 // Time Complexity --> O(N + M)
 // Space Complexity -> O(1)
 const merge = (nums1: number[], m: number, nums2: number[], n: number): void => {
-  let lastIndex = m + n - 1;
   let lastNums1 = m - 1;
   let lastNums2 = n - 1;
 
-  while (lastIndex >= 0 && lastNums2 >= 0) {
+  for (let lastIndex = m + n - 1; lastIndex >= 0 && lastNums2 >= 0; lastIndex--) {
     if (lastNums1 >= 0 && nums1[lastNums1] > nums2[lastNums2]) {
       nums1[lastIndex] = nums1[lastNums1];
       lastNums1--;
@@ -15,4 +14,4 @@ const merge = (nums1: number[], m: number, nums2: number[], n: number): void => 
       lastNums2--;
     }
   }
-}
+};
