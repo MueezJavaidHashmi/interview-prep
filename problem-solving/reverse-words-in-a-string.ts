8b3bde935844eafaa54a78812ff9f9d61c2203da
MueezJavaidHashmi <mjh1230689@gmail.com>
2025-01-18 00:32:27 +0500

    refactor(trapping-rain-water): make reverse words function using word reversal algorithm with Time Complexity O(N) and Space Complexity O(N)

--- problem-solving/reverse-words-in-a-string.ts
+++ problem-solving/reverse-words-in-a-string.ts
@@ -1,28 +1,20 @@
-// Personal solution if strings were mutable in javascript
+// Using word reversal algorithm
+// Time Complexity --> O(N)
+// Space Complexity -> O(N)
 const reverseWords = (s: string): string => {
-  for (let i = 0; i < Math.floor(s.length / 2); i++) {
-    swap(s, i, s.length - 1 - i);
-  }
-
-  let leftIndex = -1;
-  let rightIndex = -1;
+  let reversedWords = '';
+  let reversedWord = '';
 
   for (let i = 0; i < s.length; i++) {
-    if (leftIndex === -1 && s[i] !== ' ') leftIndex = i;
-    if (leftIndex > -1 && s[i] !== ' ') rightIndex = i;
+    if (s[i] !== ' ') {
+      reversedWord += s[i];
+    }
 
-    if (rightIndex > -1 && s[i] === ' ') {
-      swap(s, leftIndex, rightIndex);
-      leftIndex = -1;
-      rightIndex = -1;
+    if (reversedWord && s[i] === ' ') {
+      reversedWords = reversedWord + (reversedWords ? ' ' : '') + reversedWords;
+      reversedWord = '';
     }
   }
 
-  if (rightIndex > -1) swap(s, leftIndex, rightIndex);
-
-  return s;
+  return reversedWord ? reversedWord + (reversedWords ? ' ' : '') + reversedWords : reversedWords;
 };
-
-const swap = (s: string, index1: number, index2: number): void => {
-  [s[index1], s[index2]] = [s[index2], s[index1]];
-}
