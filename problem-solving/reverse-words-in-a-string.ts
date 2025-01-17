929b09d9e1978f4e457bee0d76ad8da298f730b1
MueezJavaidHashmi <mjh1230689@gmail.com>
2025-01-17 20:45:01 +0500

    refactor(trapping-rain-water): make reverse words function if strings were mutable in js Time Complexity O(N) and Space Complexity O(1)

--- problem-solving/reverse-words-in-a-string.ts
+++ problem-solving/reverse-words-in-a-string.ts
@@ -1,61 +1,28 @@
+// Personal solution if strings were mutable in javascript
 const reverseWords = (s: string): string => {
-  const totalWords = countWords(s);
+  for (let i = 0; i < Math.floor(s.length / 2); i++) {
+    swap(s, i, s.length - 1 - i);
+  }
 
-  return getLeftWords(s, Math.floor(totalWords / 2)) + (totalWords > 1 ? ' ' : '') + getRightWords(s, Math.ceil(totalWords / 2));
+  let leftIndex = -1;
+  let rightIndex = -1;
+
+  for (let i = 0; i < s.length; i++) {
+    if (leftIndex === -1 && s[i] !== ' ') leftIndex = i;
+    if (leftIndex > -1 && s[i] !== ' ') rightIndex = i;
+
+    if (rightIndex > -1 && s[i] === ' ') {
+      swap(s, leftIndex, rightIndex);
+      leftIndex = -1;
+      rightIndex = -1;
+    }
+  }
+
+  if (rightIndex > -1) swap(s, leftIndex, rightIndex);
+
+  return s;
 };
 
-const getLeftWords = (s: string, numberOfWords: number): string => {
-  let word = '';
-  let leftWords = '';
-  let countOfWords = 0;
-
-  for (let i = s.length - 1; i >= 0; i--) {
-    const char = s[i];
-
-    if (countOfWords >= numberOfWords) break;
-
-    if (char === ' ' && word) {
-      countOfWords++;
-      leftWords = leftWords + (leftWords ? ' ' : '') + word;
-      word = '';
-    } else {
-      word = char + word;
-    }
-  }
-
-  return leftWords;
-}
-
-const getRightWords = (s: string, numberOfWords: number): string => {
-  let word = '';
-  let rightWords = '';
-  let countOfWords = 0;
-
-  for (const char of s) {
-    if (countOfWords >= numberOfWords) break;
-
-    if (char === ' ' && word) {
-      countOfWords++;
-      rightWords = word + (rightWords ? ' ' : '') + rightWords;
-      word = '';
-    } else if (char !== ' ') {
-      word += char;
-    }
-  }
-
-  return rightWords;
-}
-
-const countWords = (s: string): number => {
-  let word = '';
-  let totalWords = 0;
-  for (const char of s) {
-    if (char === ' ' && word) {
-      totalWords++;
-      word = '';
-    } else if (char !== ' ') {
-      word += char;
-    }
-  }
-  return word ? ++totalWords : totalWords;
+const swap = (s: string, index1: number, index2: number): void => {
+  [s[index1], s[index2]] = [s[index2], s[index1]];
 }
